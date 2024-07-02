e41e03bcfae79f8d4880fa5ef5e4474e17dd554c
Adefolami <akintokunadefolami@gmail.com>
2024-07-02 01:11:14 +0100

    added some JS and some styling

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 //Selecting elements
 const dayEl = document.querySelector(".home__description--day");
 const timeEl = document.querySelector(".home__description--time");
+const navigation = document.querySelector(".navigation");
+const navEl = document.querySelectorAll(".nav__link");
 
 //Getting day of the week
 const day = new Date().getDay();
-console.log(day);
 if (day === 1) {
   dayEl.textContent = "Monday";
 } else if (day === 2) {
@@ -24,3 +25,12 @@ if (day === 1) {
 
 //Getting Time
 timeEl.textContent = `${new Date().getUTCHours()}:${new Date().getUTCMinutes()}`;
+
+//SMOOTH SCROLLING
+navigation.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
